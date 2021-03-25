cc3e4db347bdbb76c64ff72fbde98eda3f223cc9
Your Name <you@example.com>
2021-03-24 21:40:31 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,66 @@
+$(function () {
+    const tbody = $(".table tbody");
+
+    function obterDados() {
+        $.ajax('https://jsonplaceholder.typicode.com/users', {
+            type: 'GET',
+            beforeSend: function () {
+                $('.table').after('<p class="loading"> Aguarde! Carregando...</p>');
+            },
+            error: function () {
+                $('.table').after('<p class="loading"> Deu ruim </p>');
+            },
+            success: function (dados) {
+                mostrarDados(dados);
+            },
+            complete: function () {
+                $('.loading').remove();
+            }
+        })
+    }
+
+    function mostrarDados(dados) {
+        $.each(dados, function (i, el) {
+            tbody.append(`<tr class="linha">
+                            <th scope="row" class="id">${el.id}</th>
+                            <td class="nome">${el.name}</td>
+                            <td class="username">${el.username}</td>
+                            <td class="email">${el.email}</td>
+                            <td class="city"> ${el.address ? el.address.city : el.city}</td>
+                            <td class="street">Rua: ${el.address ? el.address.street : el.street}</td>
+                          </tr>                              
+         `)
+        })
+    }
+    obterDados();
+    tbody.on('click', (e) => {
+        $linha = $(e.target).closest(".linha");
+        $("#name").val($linha.children(".nome").text());
+        $("#username").val($linha.children(".username").text());
+        $("#email").val($linha.children(".email").text());
+        $("#street").val($linha.children(".street").text());
+        $("#city").val($linha.children('.city').text());
+    });
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const dados = {};
+
+        $('form').find('input').each(function (i, el) {
+            dados[el.id] = el.value;
+        });
+
+        $.ajax('https://jsonplaceholder.typicode.com/users', {
+            type: 'POST',
+            data: dados,
+            success: function (_dados) {
+                console.log(_dados)
+                mostrarDados([_dados])
+                alert('Sucesso !!!')
+            },
+            error: function () {
+                alert('Erro!!!')
+            }
+        })
+    })
+});
